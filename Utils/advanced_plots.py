6c2bb4b7956af7e9dd1d5c70cc59330c33cff53b
Kevin Rose <kevin@maypark.com>
2015-10-11 17:01:49 -0500

    Added smart labels to scatter matrix

--- Utils/advanced_plots.py
+++ Utils/advanced_plots.py
@@ -7,9 +7,10 @@ Created on Sun Oct 11 11:37:10 2015
 
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 
 # taken from http://stackoverflow.com/questions/7941207/
-# modified to plot COLUMNS first instead of rows
+# modified to plot colums instead of rows
 # modified to plot histograms down the diagonal if specified
 def scatterplot_matrix(data, names=[], hist=True, **kwargs):
     """
@@ -17,7 +18,7 @@ def scatterplot_matrix(data, names=[], hist=True, **kwargs):
     against other columns, resulting in a ncols by ncols grid of subplots with the
     diagonal subplots labeled with "names".  Additional keyword arguments are
     passed on to matplotlib's "plot" command. Returns the matplotlib figure
-    object containg the subplot grid.
+    object containing the subplot grid.
     """
     numdata, numvars = data.shape
     fig, axes = plt.subplots(nrows=numvars, ncols=numvars, figsize=(8,8))
@@ -29,14 +30,14 @@ def scatterplot_matrix(data, names=[], hist=True, **kwargs):
         ax.yaxis.set_visible(False)
 
         # Set up ticks only on one side for the "edge" subplots...
-#        if ax.is_first_col():
-#            ax.yaxis.set_ticks_position('left')
-#        if ax.is_last_col():
-#            ax.yaxis.set_ticks_position('right')
-#        if ax.is_first_row():
-#            ax.xaxis.set_ticks_position('top')
-#        if ax.is_last_row():
-#            ax.xaxis.set_ticks_position('bottom')
+        if ax.is_first_col():
+            ax.yaxis.set_ticks_position('left')
+        if ax.is_last_col():
+            ax.yaxis.set_ticks_position('right')
+        if ax.is_first_row():
+            ax.xaxis.set_ticks_position('top')
+        if ax.is_last_row():
+            ax.xaxis.set_ticks_position('bottom')
 
     if not kwargs.has_key('linestyle'):
         kwargs['linestyle'] = 'none'
@@ -51,31 +52,46 @@ def scatterplot_matrix(data, names=[], hist=True, **kwargs):
 
     # Label the diagonal subplots...
     if not names:
-        names = ['x'+str(i) for i in range(numvars)]
+        names = ['var '+str(i) for i in range(numvars)]
 
     if hist:
         for i, label in enumerate(names):
             histogram(data[:,i], ax=axes[i,i])
-            axes[0,i].set_title(label)
+            #print(dir(axes[0,i]))
+#            is_bot = ((i + 1) % 2)
+#            axes[0 - is_bot,i].set_xlabel(label)
+#            if not is_bot:
+#                axes[0,i].xaxis.set_label_position('top')
     else:
         for i, label in enumerate(names):
             axes[i,i].annotate(label, (0.5, 0.5), xycoords='axes fraction',
                     ha='center', va='center')
 
     # Turn on the proper x or y axes ticks.
-#    for i, j in zip(range(numvars), itertools.cycle((-1, 0))):
-#        axes[j,i].xaxis.set_visible(True)
-#        axes[i,j].yaxis.set_visible(True)
+    for i, j in zip(range(numvars), itertools.cycle((-1, 0))):
+        axes[j,i].xaxis.set_visible(True)
+        axes[j,i].set_xlabel(names[i])
+
+        axes[i,j].yaxis.set_visible(True)
+        axes[i,j].set_ylabel(names[i])
+
+        if j == 0:
+            axes[j,i].xaxis.set_label_position('top')
+        else:
+            axes[i,j].yaxis.set_label_position('right')
 
     # FIX #2: if numvars is odd, the bottom right corner plot doesn't have the
     # correct axes limits, so we pull them from other axes
-#    if numvars % 2:
-#        xlimits = axes[0,-1].get_xlim()
-#        ylimits = axes[-1,0].get_ylim()
-#        axes[-1,-1].set_xlim(xlimits)
-#        axes[-1,-1].set_ylim(ylimits)
+    if numvars % 2:
+        yticks = axes[-1,0].get_yticks()
+
+        currticks = axes[-1,-1].get_yticks()
+        newticks = np.linspace(min(currticks), max(currticks), len(yticks))
+        axes[-1,-1].set_yticks(newticks)
+        axes[-1,-1].set_yticklabels(__massage_ticklabels(yticks))
 
     return fig
+
 
 def histogram(data, ax=None, **kwargs):
     """
@@ -114,5 +130,20 @@ def histogram(data, ax=None, **kwargs):
     nbins = nbins / (2. * N ** (-1./3))
     edges = np.linspace(min(x), max(x), nbins+1)
 
-    ax.hist(data,bins=edges, histtype=histtype, alpha=alpha, **kwargs)
+    ax.hist(data, bins=edges, histtype=histtype, alpha=alpha, **kwargs)
 
+
+def __massage_ticklabels(labels):
+    """
+    Take an array of numbers and make turn them into strings that
+    are more presentable for tick labels.
+    """
+    ret_labels = []
+    r = max(labels) - min(labels)
+    tol = r * .001 / len(labels)
+    for label in labels:
+        if label - np.floor(label) < tol or np.ceil(label) - label < tol:
+            ret_labels.append(str(int(np.round(label))).replace(u'-', u'\u2212'))
+        else:
+            ret_labels.append(str(np.round(label,4)).replace(u'-', u'\u2212'))
+    return ret_labels
